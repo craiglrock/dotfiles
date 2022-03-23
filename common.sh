2c35c48a147ccf321ca3b360e1db5a7e7324b0a2
Craig Lehmann <craig.lehmann@shopify.com>
2022-03-23 13:58:35 -0400

    init

---
+++ common.sh
@@ -0,0 +1,5 @@
+DOTFILES_ROOT=$(pwd -P)
+
+ln -sf $DOTFILES_ROOT/.zshrc ~/.zshrc
+ln -sf $DOTFILES_ROOT/.gitconfig ~/.gitconfig
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+source ./common.sh
+
+if [ "$SPIN" ]; then
+	source ./spin.sh
+else
+fi
+
+echo -e "Player 1 Ready"
+
