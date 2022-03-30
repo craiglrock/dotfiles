8df9cbd0c01c6d819c6876000cac7b476c836d84
Craig Lehmann <craig.lehmann@shopify.com>
2022-03-30 17:56:06 -0400

    Don't override default zsh

--- common.sh
+++ common.sh
@@ -1,5 +1,4 @@
 DOTFILES_ROOT=$(pwd -P)
 
-ln -sf $DOTFILES_ROOT/.zshrc ~/.zshrc
 ln -sf $DOTFILES_ROOT/.gitconfig ~/.gitconfig
 
--- spin.sh
+++ spin.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
+echo "alias g='git'" >> ~/.zshrc
+
 sudo apt-get install -y fzf
 sudo apt-get install -y ripgrep
 sudo apt-get install -y exa
 sudo apt-get install -y fd-find
 sudo apt-get install -y bat
-
